d1eb3053437091ae86f35f78cbfb8b4c6c7da41a
unknown <elizerlumilay@gmail.com>
2023-06-02 15:49:55 +0800

    Manipulate transaction data and show it at the home view

---
+++ src/components/TransactionSingle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-transaction">
+    {{ transaction.category }}
+    {{ transaction.comments }}
+    {{ transaction.total }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['transaction']
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
 <template>
   <h1>Home</h1>
+  <div v-for="transaction in transactions" :key="transaction.id">
+    <TransactionSingle 
+      :transaction="transaction"
+    />
+  </div>
 </template>
 
 <script>
+import TransactionSingle from '../components/TransactionSingle.vue'
 
 export default {
   name: 'HomeView',
+  components: { TransactionSingle },
+  data() {
+    return {
+      transactions: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/transactions')
+      .then(res => res.json())
+      .then(data => this.transactions = data)
+      .catch(err => console.log(err))
+  }
 }
 </script>
